a6f34d248ef23a5e59076ca4bae4cecd8cef17a9
AhmadJaffal7 <72130308@students.liu.edu.lb>
2024-03-31 03:03:34 +0300

    Admin Login xml

--- app/src/main/java/com/example/cedarsvoice/MainActivity.java
+++ app/src/main/java/com/example/cedarsvoice/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.cedarsvoice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab = findViewById(R.id.fab);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "amin login page", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
