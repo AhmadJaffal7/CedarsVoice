49f596971f18142630a1efc2c0e3570b3e12f55f
AhmadJaffal7 <72130308@students.liu.edu.lb>
2024-04-13 01:59:40 +0300

    Add Fingerprint

--- app/src/main/java/com/example/cedarsvoice/AddVoterActivity.java
+++ app/src/main/java/com/example/cedarsvoice/AddVoterActivity.java
@@ -1,7 +1,7 @@
 package com.example.cedarsvoice;
 
-import android.content.Intent;
-import android.hardware.biometrics.BiometricManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
@@ -10,8 +10,11 @@ import android.view.View;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt;
+import androidx.biometric.BiometricManager;
+
 
 import android.util.Base64;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -21,9 +24,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.security.KeyStore;
-import java.security.NoSuchAlgorithmException;
-import java.security.Signature;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Executor;
@@ -57,6 +57,37 @@ public class AddVoterActivity extends AppCompatActivity {
 
     public void captureFingerprint(View view) {
         try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                BiometricManager biometricManager = BiometricManager.from(this);
+                switch (biometricManager.canAuthenticate()) {
+                    case BiometricManager.BIOMETRIC_SUCCESS:
+                        Log.d("MY_APP_TAG", "App can authenticate using biometrics.");
+                        break;
+                    case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                        Log.e("MY_APP_TAG", "No biometric features available on this device.");
+                        Toast.makeText(this, "No biometric features available on this device.", Toast.LENGTH_SHORT).show();
+                        return;
+                    case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                        Log.e("MY_APP_TAG", "Biometric features are currently unavailable.");
+                        Toast.makeText(this, "Biometric features are currently unavailable.", Toast.LENGTH_SHORT).show();
+                        return;
+                    case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                        Log.e("MY_APP_TAG", "The user hasn't associated any biometric credentials with their account.");
+                        Toast.makeText(this, "The user hasn't associated any biometric credentials with their account.", Toast.LENGTH_SHORT).show();
+                        return;
+                }
+            }else {
+                FingerprintManager fingerprintManager = (FingerprintManager) getSystemService(Context.FINGERPRINT_SERVICE);
+                if (!fingerprintManager.isHardwareDetected()) {
+                    // Device doesn't support fingerprint authentication
+                    Toast.makeText(this, "No biometric features available on this device.", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (!fingerprintManager.hasEnrolledFingerprints()) {
+                    // User hasn't enrolled any fingerprints to authenticate with
+                    Toast.makeText(this, "The user hasn't associated any biometric credentials with their account.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
             SecretKey secretKey = generateSecretKey();
             // Create a Cipher object for encryption
             Cipher cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7);
@@ -81,8 +112,8 @@ public class AddVoterActivity extends AppCompatActivity {
                         capturedFingerprintData = result.getCryptoObject().getCipher().doFinal();
                         Toast.makeText(AddVoterActivity.this, "Fingerprint captured", Toast.LENGTH_SHORT).show();
                     } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                        Toast.makeText(AddVoterActivity.this, "Error capturing fingerprint: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.e("FingerprintCapture", "Error: ", e);                    }
                 }
 
                 @Override
@@ -97,7 +128,8 @@ public class AddVoterActivity extends AppCompatActivity {
             // Start the fingerprint authentication process
             biometricPrompt.authenticate(promptInfo, new BiometricPrompt.CryptoObject(cipher));
         } catch (Exception e) {
-            e.printStackTrace();
+            Toast.makeText(this, "Error initializing fingerprint capture: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.e("FingerprintInit", "Error: ", e);
         }
     }
 
@@ -107,13 +139,24 @@ public class AddVoterActivity extends AppCompatActivity {
         String nationalID = editTextNationalID.getText().toString();
         String fingerprintData = Base64.encodeToString(capturedFingerprintData, Base64.DEFAULT);
 
+        if (firstName.isEmpty() || lastName.isEmpty() || nationalID.isEmpty() || fingerprintData.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please fill all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         String url = "http://10.0.2.2/cedarsvoice/add_voter.php";
         RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Show the ProgressBar
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        // Hide the ProgressBar
+                        progressBar.setVisibility(View.GONE);
                         if (response.trim().equals("success")) {
                             // Login successful
                             Toast.makeText(getApplicationContext(), "Voter added successfully", Toast.LENGTH_SHORT).show();
@@ -130,6 +173,8 @@ public class AddVoterActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        // Hide the ProgressBar
+                        progressBar.setVisibility(View.GONE);
                         Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
                         Log.e("VolleyError",error.toString());
                     }
